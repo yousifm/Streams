688fc5b6bd33a01507039f95351bc22701bcb193
yousifm <ymahmoud1b@gmail.com>
2019-09-15 00:26:17 +0800

    Implemented rtmp server

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
+
 import { fetchStream } from "../../actions";
 import Loader from "../Loader";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -17,6 +44,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
